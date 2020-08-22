0b2abbc7d97508d163211f15d7d6153543136dfb
Alex <grok88@tut.by>
2020-08-22 21:26:50 +0300

    Update - Big refactor own Select

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -8,26 +8,31 @@ export default {
 }
 
 const items = [
-    {title: "Alex", value: 1},
-    {title: "Gor", value: 2},
-    {title: "Gora", value: 3},
-    {title: "Anyfriy", value: 4},
+    {title: "Alex", value: '1'},
+    {title: "Gor", value: '2'},
+    {title: "Jora", value: '3'},
+    {title: "Anyfriy", value: '4'},
 ]
 
 const collapsed = action('Select is collapsed');
-const unCollapsed = action('Select is collapsed');
-const selectTitle = action('selectTitle');
 
 export const CollapsedMode = () => {
-    return <Select items={items} value={1} onChange={collapsed} collapsed={true} selectTitle={collapsed}/>
+    return <Select items={items} value={'1'} onChange={collapsed}/>
 }
-export const UnCollapsedMode = () => {
-    const [value, setValue] = useState<number>(1);
-    return <Select items={items} value={value} onChange={unCollapsed} collapsed={false} selectTitle={setValue}/>
-}
-export const ModeChanging = () => {
-    const [collapsed, setCollapsed] = useState<boolean>(true);
-    const [value, setValue] = useState<number>(1);
 
-    return <Select items={items} value={value} onChange={setCollapsed} collapsed={collapsed} selectTitle={setValue}/>
+export const UnCollapsedMode = () => {
+    const [value, setValue] = useState<string>('1');
+    return <Select items={items} value={value} onChange={setValue}/>
+}
+
+export const ModeChangingWithValue = () => {
+    const [value, setValue] = useState<string>('2');
+
+    return <Select items={items} value={value} onChange={setValue}/>
+}
+
+export const ModeChangingWithoutValue = () => {
+    const [value, setValue] = useState<string>('1');
+
+    return <Select items={items} onChange={setValue}/>
 }
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,31 +1,124 @@
-import React from "react";
+import React, {useState, KeyboardEvent, useEffect} from "react";
 import styles from './Select.module.css';
 
 type ItemsType = {
     title: string;
-    value: number;
+    value: string;
 }
 type SelectPropsType = {
     items: ItemsType[];
-    value: number;
+    value?: string;
     onChange: (value: any) => void;
-    collapsed: boolean;
-    selectTitle:(value:number) => void;
+    // collapsed: boolean;
+    // selectTitle: (value: string) => void;
 }
 
 export const Select = (props: SelectPropsType) => {
-    const {items, onChange, value, collapsed, selectTitle} = props;
-    const item = items.find(item => item.value === value);
-    const title = item && item.title;
-    const  onSelectTitle = (item:ItemsType) => {
-        selectTitle(item.value)
-        onChange(!collapsed)
-    }
+    const {items, onChange, value} = props;
 
+    const [active, setActive] = useState<boolean>(false);
+    const [hoveredItemValue, setHoveredItemValue] = useState<string | undefined>(value);
+
+    const selectedItem = items.find(item => item.value === hoveredItemValue);
+    // const hoveredItem = items.find(item => item.value === hoveredItemValue);
+
+    useEffect(() => {
+        setHoveredItemValue(value);
+    }, [value])
+
+    const title = selectedItem && selectedItem.title;
+
+    const toggleItems = () => setActive(!active);
+    const onItemSelect = (value: string) => {
+        onChange(value);
+        setActive(false);
+    }
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].value === hoveredItemValue) {
+                    const Element = e.key === 'ArrowDown'
+                        ? items[i + 1]
+                        : items[i - 1]
+                    if (Element) {
+                        onChange(Element.value);
+                        return;
+                    }
+                }
+            }
+            if (!selectedItem) {
+                onChange(items[0].value);
+            }
+        }
+        if (e.key === 'Escape' || e.key === 'Enter') {
+            setActive(false);
+        }
+        console.log('keyUp')
+    }
     return (
-        <div className={styles.selectBlock}>
-            <div className={styles.title} onClick={() => onChange(!collapsed)}>{title}</div>
-            {!collapsed && items.map((item, i) => <div className={styles.item} onClick={() =>  onSelectTitle(item)} key={i}>{item.title}</div>)}
-        </div>
+        <>
+            <select name="" id="">
+                <option value="1">Alex</option>
+                <option value="2">Gor</option>
+                <option value="3">Jora</option>
+                <option value="4">Anyfriy</option>
+            </select>
+
+            <div className={styles.selectBlock} tabIndex={0} onKeyUp={onKeyUp}>
+                <span className={styles.title} onClick={toggleItems}>{title}</span>
+                {
+                    active && <div className={`${styles.items}`}>
+                        {items.map((item, i) => <div
+                            onMouseEnter={() => setHoveredItemValue(item.value)}
+                            className={styles.item + ' ' + (selectedItem === item ? styles.selected : '')}
+                            key={i}
+                            onClick={() => onItemSelect(item.value)}>{item.title}</div>)}
+					</div>
+                }
+
+            </div>
+
+        </>
+
     );
 }
+
+
+// export const Select = (props: SelectPropsType) => {
+//     const {items, onChange, value, collapsed, selectTitle} = props;
+//
+//     const [active, setActive] = useState<boolean>(true)
+//
+//     const item = items.find(item => item.value === value);
+//     const title = item && item.title;
+//
+//     const onSelectTitle = (item: ItemsType) => {
+//         selectTitle(item.value);
+//         onChange(!collapsed);
+//     }
+//
+//     return (
+//         <>
+//             <select name="" id="">
+//                 <option value="1">Alex</option>
+//                 <option value="2">Gor</option>
+//                 <option value="3">Jora</option>
+//                 <option value="4">Anyfriy</option>
+//             </select>
+//
+//             <div className={styles.selectBlock + '' + styles.active}>
+//                 <h3 className={styles.title} onClick={() => onChange(!collapsed)}>{title}</h3>
+//                 {
+//                     active && <div className={`${styles.items}`}>
+//                         {!collapsed && items.map((item, i) => <div className={styles.item}
+//                                                                    onClick={() => onSelectTitle(item)}
+//                                                                    key={i}>{item.title}</div>)}
+// 					</div>
+//                 }
+//
+//             </div>
+//
+//         </>
+//
+//     );
+// }
