bb2980b90802a4e496edb817ec05c3b4c76bbea4
Alex <grok88@tut.by>
2020-10-08 13:59:00 +0300

    21 - useEffect \ setTimeout \ setInterval

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -20,21 +20,67 @@ export const SimpleUseEffectExample = () => {
     useEffect(() => {
         console.log('useEffect only first render');
         document.title = count.toString();
-    },[]);
+    }, []);
 
     useEffect(() => {
         console.log('useEffect  first render - and every count change');
         document.title = count.toString();
-    },[count]);
-
+    }, [count]);
 
 
     return (
         <>
-            <button onClick={() => {setCount(count + 1)}}>Count +</button>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>Count +
+            </button>
             {count}
-            <button onClick={() => {setFake(fake + 1)}}>Fake +</button>
+            <button onClick={() => {
+                setFake(fake + 1)
+            }}>Fake +
+            </button>
             {fake}
+        </>
+    );
+}
+
+export const SetTimeooutUseEffectExample = () => {
+
+    console.log('SetTimeooutUseEffectExample');
+
+    const [count, setCount] = useState<number>(1);
+    const [fake, setFake] = useState<number>(1);
+
+    // useEffect(() => {
+    //
+    //     setTimeout(() => {
+    //         console.log('setTimeout');
+    //         document.title = count.toString();
+    //     }, 1000)
+    // },[count]);
+
+    useEffect(() => {
+
+        setInterval(() => {
+            console.log('tick : ' + count )
+            setCount(state => state + 1)
+        }, 1000)
+
+    }, []);
+
+
+    return (
+        <>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>Count +
+            </button>
+            Counter: {count}
+            <button onClick={() => {
+                setFake(fake + 1)
+            }}>Fake +
+            </button>
+            Fake: {fake}
         </>
     );
 }
